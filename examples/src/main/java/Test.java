005764a828742951129b39c05102a35fd95ec165
MarLeo <martin.aurele12@gmail.com>
2017-12-30 19:42:54 +0100

    first commit
    Signed-off-by: MarLeo <martin.aurele12@gmail.com>

---
+++ examples/src/main/java/Test.java
@@ -0,0 +1,13 @@
+import com.tchokonthe.swagger.model.User;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        User user = new User(1, "Martin Tchokonthe", 27, 42400);
+        System.out.println("User Info: " + user);
+
+    }
+
+
+}
