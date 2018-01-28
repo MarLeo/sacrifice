116257aed4b1b0d2ed4f6a529153265ad3b6e976
MarLeo <martin.aurele12@gmail.com>
2018-01-28 17:20:28 +0100

    Generics first course
    Signed-off-by: MarLeo <martin.aurele12@gmail.com>

--- examples/src/main/java/ExcelDriver.java
+++ examples/src/main/java/ExcelDriver.java
@@ -1,11 +1,8 @@
-import dao.ComponentsDao;
-import model.Components;
-import model.Enclosures;
+import generics.CircularBuffer;
+import model.Person;
 import org.apache.log4j.Logger;
-import util.ExcelUtilities;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.Arrays;
 
 public class ExcelDriver {
 
@@ -13,18 +10,18 @@ public class ExcelDriver {
 
     private static final String path = "catalogTest.xlsx";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        File file = ExcelUtilities.readResource(path);
+        //File file = ExcelUtilities.readResource(path);
 
-        logger.info(String.format("File Name: %s", file.getName()));
+        // logger.info(String.format("File Name: %s", file.getName()));
 
         /* ChienDao dao =  new PoiChienDao(file.getAbsolutePath());
 
         logger.info( String.format("Liste d'animaux: %s",  dao.findAllChiens()));
         */
 
-        Components components = new Components.Builder()
+        /*Components components = new Components.Builder()
                 .componentType("Electrical Component")
                 .reference("14811")
                 .description("COMB BUSBAR 24 PITCHES- FOR C120 AND NG125- 63A UNI")
@@ -33,7 +30,6 @@ public class ExcelDriver {
                 .build();
 
         logger.info(String.format("Components data: %s", components));
-
         Enclosures enclosures = new Enclosures.Builder<>()
                 .componentType("Electrical Component")
                 .reference("14811")
@@ -54,8 +50,50 @@ public class ExcelDriver {
 
         ComponentsDao dao = new ComponentsDao(file.getAbsolutePath());
         dao.findAllComponents();
+        */
         //logger.info( String.format("Liste de composants: %s",  dao.findAllComponents()));
 
+        /*CircularBuffer<String> buffer = new CircularBuffer<>(10);
+
+        buffer.offer("a");
+        buffer.offer("bc");
+        buffer.offer("d");
+
+        System.out.println("result = " + concatenate(buffer));*/
+
+
+        Person p1 = new Person("Don Draper", 89);
+        Person p2 = new Person("Peggy Olson", 65);
+
+        Person[] madMen = {p1, p2};
+
+        Person p3 = new Person("Bert Cooper", 100);
+
+        madMen = add(p3, madMen);
+
+
+        System.out.println(Arrays.toString(madMen));
+
+
+    }
+
+    private static Person[] add(final Person p3, Person[] array) {
+
+        final int length = array.length;
+        array = Arrays.copyOf(array, length + 1);
+        array[length] = p3;
+        return array;
+    }
+
+    private static String concatenate(CircularBuffer<String> buffer) {
+        StringBuilder result = new StringBuilder();
+
+        String value;
+        while ((value = buffer.poll()) != null) {
+            result.append(value);
+        }
+
+        return result.toString();
     }
 
 }
---
+++ examples/src/main/java/model/Person.java
@@ -0,0 +1,40 @@
+package model;
+
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
+public final class Person {
+
+
+    private final String name;
+    private final int age;
+
+
+    public Person(String name, int age) {
+
+        Objects.requireNonNull(name);
+        this.name = name;
+        this.age = age;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return age == person.getAge() && name == person.getName();
+    }
+
+
+}
